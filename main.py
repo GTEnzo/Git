a3683fc51b1d9c1a0828d6a02416de7bd2288f45
Enzo <tsopanovenzo@gmail.com>
2025-01-18 15:15:51 +0300

    def paintEvent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QApplication
+import random
 from PyQt6 import uic
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtGui import QPainter, QColor
 
 
 class Program(QWidget):
@@ -8,9 +10,24 @@ class Program(QWidget):
         super().__init__()
         uic.loadUi('UI.ui', self)
         self.btn.clicked.connect(self.click)
+        self.flag = False
 
     def click(self):
-        print('ok')
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+            self.flag = False
+
+    def draw(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        num = random.randint(10, 250)
+        qp.drawEllipse(250 - num // 2, 250 - num // 2, num, num)
 
 
 if __name__ == '__main__':
