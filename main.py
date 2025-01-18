8152afc3a352e9c5e0052eaf80f30e0616d8e272
Enzo <tsopanovenzo@gmail.com>
2025-01-18 14:44:27 +0300

    class

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import sys
 from PyQt6.QtWidgets import QWidget, QApplication
 from PyQt6 import uic
+
+
+class Program(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    program = Program()
+    program.show()
+    sys.exit(app.exec())
