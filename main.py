fab7dce92c11b1e76571b16050777d302a17f0a3
Enzo <tsopanovenzo@gmail.com>
2025-02-14 11:21:43 +0300

    4

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import sys
 import sqlite3
-from PyQt6.QtWidgets import QWidget, QApplication
 from PyQt6 import uic
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
 
 
-class Program(QWidget):
+class CoffeeApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.btn.clicked.connect(self.click)
-        self.num = 0
-
-    def click(self):
-        if self.num < 5:
-            self.num += 1
-        else:
-            self.num = 1
+        uic.loadUi('main.ui', self)
         con = sqlite3.connect('coffee.sqlite')
         cur = con.cursor()
-        res = cur.execute(f'''SELECT * FROM coffees WHERE id == {self.num}''').fetchall()
-        self.textBrowser.setText(
-            f'<b>{res[0][0]}: {res[0][1]}, {res[0][2]}, {res[0][3]}, {res[0][4]}, {res[0][5]}, {res[0][6]}<b>')
+        cur.execute("SELECT * FROM coffee")
+        rows = cur.fetchall()
+        self.tableWidget.setRowCount(len(rows))
+        self.tableWidget.setColumnCount(len(rows[0]) if rows else 0)
+        for row_index, row_data in enumerate(rows):
+            for column_index, item in enumerate(row_data):
+                self.tableWidget.setItem(row_index, column_index, QTableWidgetItem(str(item)))
+        con.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    program = Program()
-    program.show()
+    window = CoffeeApp()
+    window.show()
     sys.exit(app.exec())
